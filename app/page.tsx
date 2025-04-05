2562e4d35c3a8ad445908a9b584f7f914a7eb4b3
takeokunn <bararararatty@gmail.com>
2025-04-05 21:33:13 +0900

    fix: correct import paths and update TypeScript reference link

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@
 
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+import type { Schema } from "..//amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+import outputs from "../amplify_outputs.json";
+import "aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
